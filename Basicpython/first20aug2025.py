90ee4da98cfd1da9fdc914d24f229bca7d3d7619
Khushi-var10 <nehavar1018@gmail.com>
2025-08-24 11:59:47 +0530

    First program in python

---
+++ Basicpython/first20aug2025.py
@@ -0,0 +1,7 @@
+firstname = input("Please enter first name: ")
+lastname = input("Please enter last name: ")
+contact = int(input("Please enter contact number: "))
+address = input("Please enter address: ")
+age = int(input("Please enter age: "))
+
+print(f"\nOur detail:\n{firstname}, \n{lastname}, \n{contact}, \n{address}, \n{age}")
