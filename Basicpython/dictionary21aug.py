6be6889b27d47c0a325e81f8045fb212dbd43633
Khushi-var10 <nehavar1018@gmail.com>
2025-08-24 18:27:26 +0530

    Dictionary data

---
+++ Basicpython/dictionary21aug.py
@@ -0,0 +1,24 @@
+country_details = {}
+
+country_details["india"]="new delhi"
+country_details["china"]="beijing"
+country_details["united states"]="washington,D.C"
+country_details["russia"]="moscow"
+country_details["germany"]="berlin"
+country_details["japan"]="tokyo"
+country_details["france"]="paris"
+country_details["united kingdom"]="london"
+country_details["brazil"]="brasilia"
+country_details["canada"]="ottawa"
+country_details["italy"]="rome"
+country_details["spain"]="madrid"
+country_details["australia"]="canberra"
+country_details["mexico"]="mexcio city"
+country_details["indonesia"]="jakarta"
+country_details["south korea"]="seoul"
+country_details["saudi arabia"]="riyadh"
+country_details["argentina"]="buenos aries"
+country_details["egypt"]="cairo"
+country_details["turkey"]="ankara"
+
+print(country_details)
