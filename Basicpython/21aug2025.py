fe3595a94d00d73e932536026a9556253a342684
Khushi-var10 <nehavar1018@gmail.com>
2025-08-24 18:09:22 +0530

    new code

---
+++ Basicpython/21aug2025.py
@@ -0,0 +1,20 @@
+studentdata={}
+
+studentdata["id"]=int(input("Please enter student id: "))
+studentdata["name"]=input("Please enter student name: ")
+studentdata["contact"]=int(input("Please enter student contact: "))
+studentdata["address"]=input("Please enter student address: ")
+studentdata["email"]=input("Please enter student email: ")
+studentdata["age"]=int(input("Please enter student age: "))
+studentdata["qualification"]=input("Please enter student qualification: ")
+studentdata["state"]=input("Please enter student state: ")
+studentdata["city"]=input("Please enter student city: ")
+studentdata["date-of-birth"]=input("Please enter student date-of-birth: ")
+studentdata["gender"]=input("Please enter student genter: ")
+studentdata["pincode"]=int(input("Please enter student pincode: "))
+
+#del studentdata["id"]
+#del studentdata['name']
+#print("Contact", studentdata['contact'])
+print(studentdata)
+
